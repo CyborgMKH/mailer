fbccaecefb7b43550b9fe670b07ca3e333090fee
cyborgmkh <sumanmukhiya@ismt.edu.np>
2023-07-12 22:43:11 +0545

    changes done in cors

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(limiter);
