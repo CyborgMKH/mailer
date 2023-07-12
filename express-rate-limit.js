26fb7e4e6adfcd5e16cfd3c4206643c4f617603f
cyborgmkh <sumanmukhiya@ismt.edu.np>
2023-07-12 12:45:19 +0545

    adding mailers file to github

---
+++ express-rate-limit.js
@@ -0,0 +1,12 @@
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Maximum 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Maximum send request reached ! Try after 15 min.",
+});
+
+module.exports = limiter;
+
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const limiter = require("./express-rate-limit");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(limiter);
+
+app.post("/send-email", (req, res) => {
+  // Extract the form data
+  const { name, email, message } = req.body;
+
+  // Create a transporter using Zoho Mail SMTP settings
+  const transporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.USER_EMAIL,
+      pass: process.env.USER_PASSWORD,
+    },
+  });
+
+  // Compose the email message
+  const mailOptions = {
+    from: email,
+    to: process.env.USER_EMAIL,
+    subject: "New Contact Form Submission",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ error: "An error occurred while sending the email." });
+    } else {
+      console.log("Email sent:", info.response);
+      res.json({ message: "Email sent successfully." });
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
